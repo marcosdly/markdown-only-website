14a01abfba8399a0d0e528270bac418d5cb195b1
Marcos Delay <dev.marcosdly@gmail.com>
2024-03-10 09:10:46 -0300

    fix: scroll and background color

--- lib/common/article.tsx
+++ lib/common/article.tsx
@@ -8,12 +8,10 @@ export function Article({ md }: ArticleProps) {
   const markdownParser = markdown();
 
   return (
-    <div id="article-container">
-      <div
-        id="article"
-        className="lexend-font"
-        dangerouslySetInnerHTML={{ __html: markdownParser.render(md) }}
-      ></div>
-    </div>
+    <div
+      id="article"
+      className="lexend-font"
+      dangerouslySetInnerHTML={{ __html: markdownParser.render(md) }}
+    ></div>
   );
 }

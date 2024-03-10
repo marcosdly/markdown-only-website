a26cada953b5529d60e2541a7659f1ae382a536c
Marcos Delay <dev.marcosdly@gmail.com>
2024-03-10 07:34:32 -0300

    refactor(markdown): fix dependencies and parsing, plus example file

--- lib/markdown/markdown.tsx
+++ lib/markdown/markdown.tsx
@@ -1,35 +1,17 @@
-import dompurify from "dompurify";
-import hljs from "highlight.js";
-import { Marked } from "marked";
-import { baseUrl } from "marked-base-url";
-import { createDirectives } from "marked-directive";
-import markedFootnote from "marked-footnote";
-import { markedHighlight } from "marked-highlight";
-import { markedSmartypants } from "marked-smartypants";
-import { imageDirectives } from "./directives/image";
+import MarkdownIt from "markdown-it";
+// @ts-ignore
+import deflist from "markdown-it-deflist";
+// @ts-ignore
+import { full as emoji } from "markdown-it-emoji";
+// @ts-ignore
+import footnote from "markdown-it-footnote";
+// @ts-ignore
+import sub from "markdown-it-sub";
+// @ts-ignore
+import sup from "markdown-it-sup";
 
-export class Markdown {
-  marked: Marked;
-
-  constructor() {
-    this.marked = new Marked(
-      baseUrl(window.origin),
-      markedFootnote(),
-      markedSmartypants(),
-      markedHighlight({
-        langPrefix: "hljs language-",
-        highlight(code, lang, info) {
-          const language = hljs.getLanguage(lang) ? lang : "plaintext";
-          return hljs.highlight(code, { language }).value;
-        },
-      }),
-      createDirectives([...imageDirectives]),
-    );
-  }
-
-  async parse(md: string): Promise<string> {
-    return dompurify.sanitize(await this.marked.parse(md), {
-      USE_PROFILES: { html: true },
-    });
-  }
+export function markdown(): MarkdownIt {
+  const markdownit = MarkdownIt();
+  markdownit.use(footnote).use(emoji).use(deflist).use(sub).use(sup);
+  return markdownit;
 }

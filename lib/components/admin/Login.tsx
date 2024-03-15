7d8f7b31acda8b635f2206349fd0be9c03446547
Marcos Delay <dev.marcosdly@gmail.com>
2024-03-15 06:49:19 -0300

    chore: login and buttons style

--- lib/components/admin/Login.tsx
+++ lib/components/admin/Login.tsx
@@ -3,14 +3,21 @@ function verify(ev: MouseEvent) {
 }
 
 export default function Login() {
+  const inputClasses = "p-1 rounded bg-dark border border-primary text-white";
   return (
     <div className="local-login-container">
-      <form action="login" method="post">
-        <label htmlFor="email">Enter authorized email</label>
-        <input type="text" name="email" />
+      <form
+        action="login"
+        method="post"
+        className="d-grid gap-2 border rounded border-primary p-3"
+      >
+        <label htmlFor="email" className="p-1">
+          Email
+        </label>
+        <input type="text" name="email" className={inputClasses} />
         <label htmlFor="passwd">Password</label>
-        <input type="text" name="passwd" />
-        <input type="submit" value="Try" onClick={verify} />
+        <input type="text" name="passwd" className={inputClasses} />
+        <input type="submit" value="Try" onClick={verify} className="btn btn-primary" />
       </form>
     </div>
   );

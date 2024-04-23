4998c3a19ef4dd99b7c43bbaf9a622c5c7e52ce3
Marcos Delay <dev.marcosdly@gmail.com>
2024-04-23 13:10:18 -0300

    Another start....I'm so tired of this shii smh

--- lib/admin/MainContext.tsx
+++
@@ -1,5 +0,0 @@
-import { VNode, createContext } from "preact";
-import { StateUpdater } from "preact/hooks";
-
-const MainContext = createContext<StateUpdater<VNode>>({} as StateUpdater<VNode>);
-export default MainContext;
